b349115107b20667495c66b1b2ce81dff324c122
Gerald Bullard Jr <geraldbullard@purplerockscissors.com>
2022-04-28 11:37:45 -0400

    Added inputRef, animation, cleanup, styling, removed country.

--- src/components/WeatherCard/Location.js
+++ src/components/WeatherCard/Location.js
@@ -1,48 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "@emotion/styled";
+import { motion } from "framer-motion";
 
-const Location = ({ city, country, getWeather }) => {
+const Location = ({ city, getWeather }) => {
     const [query, setQuery] = useState(""); 
     const [inputCityMode, setInputCityMode] = useState(false);
-    const [inputCityFocusState, setInputCityFocusState] = useState(false);
+    const inputCityRef = useRef("");
+    // Another way to focus not in videos
+    //const [inputCityFocusState, setInputCityFocusState] = useState(false);
+
+    useEffect(() => {
+        if (inputCityMode) {
+            inputCityRef.current.focus();
+        }
+    }, [inputCityMode]);
+    
+    if (inputCityMode) {
+        return (
+            <Container>
+                <div className="formWrapper">
+                    <motion.div initial={{opacity:0}} animate={{opacity:1}}></motion.div>
+                        <FormElement onSubmit={e => {
+                            e.preventDefault();
+                            getWeather(query);
+                        }}>
+                            <p className="city-input">
+                                <InputField
+                                    className="city-input"
+                                    id="city_input"
+                                    required
+                                    ref={inputCityRef}
+                                    /*autoFocus={inputCityFocusState}*/ // Another way to focus not in videos
+                                    value={query}
+                                    onChange={e => setQuery(e.target.value)}
+                                />
+                            </p>
+                            <p className="city-input-controls">
+                                <SearchButton type="submit">Search</SearchButton>
+                                <CancelButton onClick={() => setInputCityMode(false)}>X</CancelButton>
+                            </p>
+                        </FormElement>
+                    </motion.div>
+                </div>
+            </Container>
+        );
+    }
+    
     return (
         <Container>
-            {!inputCityMode &&
-                <City onClick={() => {
-                    setInputCityMode(true);
-                    setInputCityFocusState(true);
-                }}>{city}</City>
-            }
-            {inputCityMode &&
-            <div className="formWrapper">
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    getWeather(query);
-                }}>
-                    <p className="city-input">
-                        <input
-                            className="city-input"
-                            id="city_input"
-                            required
-                            autoFocus={inputCityFocusState}
-                            placeholder="City Name"
-                            value={query}
-                            onChange={e => setQuery(e.target.value)}
-                        />
-                    </p>
-                    <p className="city-input-controls">
-                        <button type="submit">Search</button>&nbsp;
-                        <button onClick={() => setInputCityMode(false)}>Cancel</button>
-                    </p>
-                </form>
-            </div>
-            }
-            <Country>{country}</Country>
+            <City onClick={() => {
+                setInputCityMode(true);
+                /*setInputCityFocusState(true);*/ // Another way to focus not in videos
+            }}>{city}</City>
         </Container>
     );
 }
  
 export default Location;
+
+const FormElement = styled.form`
+    display: flex;
+    position: relative;
+    background-color: #46618b;
+    border-radius: 5px;
+    margin-left: -5px;
+`;
+const InputField = styled.input`
+    padding: 6px;
+    width: 90px;
+    background: transparent;
+    border: none;
+        color: white;
+    &:focus {
+        outline: 0;
+    }
+`;
+const SearchButton = styled.button`
+    background: #394e70;
+    color: white;
+    border: none;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    padding: 6px;
+    cursor: pointer;
+`;
+const CancelButton = styled.span`
+    position: absolute;
+    top: -8px;
+    right: -13px;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: #557fc2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 0.8rem;
+    box-shadow: 2px 1px 3px rgb(0,0,0, 0.4);
+`;
 
 const Container = styled.div`
     text-align: center;
@@ -56,8 +112,4 @@ const City = styled.h1`
     &:hover {
         top: -3px;
     }
-`;
-const Country = styled.h3`
-    font-family: sans-serif;
-    font-size: 1.1rem;
 `;
